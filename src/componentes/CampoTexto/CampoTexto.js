f7e632f38ef30ee932f31dc6b4898eb3b4eb3e92
= <thiagoespindula00@gmail.com>
2022-10-12 20:07:10 -0300

    Commit inicial. Criado os componentes Banner e CampoTexto

---
+++ src/componentes/CampoTexto/CampoTexto.js
@@ -0,0 +1,10 @@
+import "./CampoTexto.css";
+
+export const CampoTexto = () => {
+    return (
+        <div className="campo_texto">
+            <label>Nome</label>
+            <input placeholder="Digite seu o nome"/>
+        </div>
+    );
+}
