e3146fce8c3ef8262a24f64e49831967dfbad0b1
= <thiagoespindula00@gmail.com>
2022-10-15 14:38:59 -0300

    Condicionado para listar apenas times que possuem colaboradores

--- src/componentes/Time/Time.js
+++ src/componentes/Time/Time.js
@@ -12,7 +12,7 @@ export const Time = (props) => {
     }
 
     return(
-        <section className="time" style={cssStyleSection}>
+        props.colaboradores.length > 0 && <section className="time" style={cssStyleSection}>
             <h3 style={cssStyleH3}>{props.nome}</h3>
             <div className='colaboradores'>
                 {props.colaboradores.map(colaborador => <CardColaborador
