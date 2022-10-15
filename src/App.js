b5335e5fc4cf8e60433a0ff4ede46a46fff72fda
= <thiagoespindula00@gmail.com>
2022-10-15 11:24:42 -0300

    Criado o componente Time

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
-import Banner     from './componentes/Banner';
-import Formulario from './componentes/Formulario';
+import Banner       from './componentes/Banner';
+import Formulario   from './componentes/Formulario';
+import Time         from './componentes/Time';
 
 function App() {
 
@@ -15,6 +16,8 @@ function App() {
     <div className="App">
       <Banner />
       <Formulario aoCadastrarNovoColaborador={adicionaNovoColaborador}/>
+      <Time nome="Programação" />
+      <Time nome="Front-end"/>
     </div>
   );
 }
